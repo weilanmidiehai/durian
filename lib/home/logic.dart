9e5346a47e9adbc51ad54f7c717962ced1fe6afb
Lily <103798802+xuel59565@users.noreply.github.com>
2023-09-11 10:13:29 +0800

    Scaffold 的抽屉Drawer

--- lib/home/logic.dart
+++ lib/home/logic.dart
@@ -1,6 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeLogic extends GetxController {
+
+  int selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void onItemTapped(int index) {
+    selectedIndex = index;
+    update();
+  }
   @override
   void onReady() {
     // TODO: implement onReady
--- lib/home/view.dart
+++ lib/home/view.dart
@@ -11,24 +11,71 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Du',
-        ),
-      ),
-      body: Column(
-        children: [
-          const Text('data'),
-          ElevatedButton(
-              onPressed: () {
-                Get.isDarkMode
-                    ? Get.changeTheme(greenTheme)
-                    : Get.changeTheme(darkGreenTheme);
-              },
-              child: const Text('变换主题'))
-        ],
-      ),
-    );
+    return GetBuilder<HomeLogic>(
+        init: logic,
+        builder: (controller) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                'Du',
+              ),
+            ),
+            drawer: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  DrawerHeader(
+                    child: Center(
+                      child: ClipOval(
+                        child: Image.network(
+                          "https://cdn.pixabay.com/photo/2023/08/18/15/02/dog-8198719_1280.jpg",
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text('Home'),
+                    selected: logic.selectedIndex == 0,
+                    onTap: () {
+                      // Update the state of the app
+                      logic.onItemTapped(0);
+                      // Then close the drawer
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: Text(Get.isDarkMode ? '白天模式' : '夜晚模式'),
+                    selected: logic.selectedIndex == 1,
+                    onTap: () {
+                      Get.isDarkMode
+                          ? Get.changeTheme(greenTheme)
+                          : Get.changeTheme(darkGreenTheme);
+                      logic.onItemTapped(1);
+                      // Then close the drawer
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('School'),
+                    selected: logic.selectedIndex == 2,
+                    onTap: () {
+                      logic.onItemTapped(2);
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+            body: Column(
+              children: [
+                const Text('data'),
+                HomeLogic.widgetOptions[logic.selectedIndex],
+              ],
+            ),
+          );
+        });
   }
 }
